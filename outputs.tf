03cb78d15254588beeb83326515f12a58b1bfe7d
Miguel Bernadin <Miguel.Bernadin@gmail.com>
2018-09-27 14:20:36 -0700

    terraform fmt after description add

--- outputs.tf
+++ outputs.tf
@@ -1,95 +1,95 @@
 # Number of Instance
 output "num_masters" {
   description = "Specify the amount of masters. For redundancy you should have at least 3"
-  value = "${var.num_masters}"
+  value       = "${var.num_masters}"
 }
 
 # Cluster Name
 output "name_prefix" {
   description = "Cluster Name"
-  value = "${var.name_prefix}"
+  value       = "${var.name_prefix}"
 }
 
 # Instance Type
 output "instance_type" {
   description = "instance type"
-  value = "${var.instance_type}"
+  value       = "${var.instance_type}"
 }
 
 # DCOS Version for prereq install
 output "dcos_version" {
   description = "Specifies which DC/OS version instruction to use. Options: 1.9.0, 1.8.8, etc. See dcos_download_path or dcos_version tree for a full list."
-  value = "${var.dcos_version}"
+  value       = "${var.dcos_version}"
 }
 
 # Tested OSes to install with prereq
 output "dcos_instance_os" {
   description = "Operating system to use. Instead of using your own AMI you could use a provided OS."
-  value = "${var.dcos_instance_os}"
+  value       = "${var.dcos_instance_os}"
 }
 
 # Source image to boot from
 output "image" {
   description = "image"
-  value = "${var.image}"
+  value       = "${var.image}"
 }
 
 # Disk Type to Leverage
 output "disk_type" {
   description = "Disk Type to Leverage."
-  value = "${var.disk_type}"
+  value       = "${var.disk_type}"
 }
 
 # Disk Size in GB
 output "disk_size" {
   description = "disk size"
-  value = "${var.disk_size}"
+  value       = "${var.disk_size}"
 }
 
 # Resource Group Name
 output "resource_group_name" {
   description = "resource group name"
-  value = "${var.resource_group_name}"
+  value       = "${var.resource_group_name}"
 }
 
 # Customer Provided Userdata
 output "user_data" {
   description = "User data to be used on these instances (cloud-init)"
-  value = "${var.user_data}"
+  value       = "${var.user_data}"
 }
 
 # SSH User
 output "admin_username" {
   description = "admin username"
-  value = "${module.dcos-master-instances.admin_username}"
+  value       = "${module.dcos-master-instances.admin_username}"
 }
 
 # SSH Public Key
 output "public_ssh_key" {
   description = "public ssh key"
-  value = "${var.public_ssh_key}"
+  value       = "${var.public_ssh_key}"
 }
 
 # Private IP Addresses
 output "private_ips" {
   description = "List of private ip addresses created by this module"
-  value = ["${module.dcos-master-instances.private_ips}"]
+  value       = ["${module.dcos-master-instances.private_ips}"]
 }
 
 # Public IP Addresses
 output "public_ips" {
   description = "List of public ip addresses created by this module"
-  value = ["${module.dcos-master-instances.public_ips}"]
+  value       = ["${module.dcos-master-instances.public_ips}"]
 }
 
 # Master Load Balancer Address
 output "lb.fqdn" {
   description = "lb.fqdn"
-  value = "${module.master-lb.elb_address}"
+  value       = "${module.master-lb.elb_address}"
 }
 
 # Returns the ID of the prereq script
 output "prereq_id" {
   description = "prereq id"
-  value = "${module.dcos-master-instances.prereq_id}"
+  value       = "${module.dcos-master-instances.prereq_id}"
 }
--- variables.tf
+++ variables.tf
@@ -34,6 +34,7 @@ variable "dcos_instance_os" {
 # Private SSH Key Filename Optional
 variable "ssh_private_key_filename" {
   description = "Path to the SSH private key"
+
   # cannot leave this empty as the file() interpolation will fail later on for the private_key local variable
   # https://github.com/hashicorp/terraform/issues/15605
   default = "/dev/null"
@@ -47,7 +48,7 @@ variable "image" {
 # Disk Type to Leverage. The managed disk type. (optional)
 variable "disk_type" {
   description = "Disk Type to Leverage."
-  default = "Standard_LRS"
+  default     = "Standard_LRS"
 }
 
 # Disk Size in GB
@@ -63,7 +64,7 @@ variable "resource_group_name" {
 # Customer Provided Userdata
 variable "user_data" {
   description = "User data to be used on these instances (cloud-init)"
-  default = ""
+  default     = ""
 }
 
 # SSH User
@@ -79,20 +80,20 @@ variable "public_ssh_key" {
 # Allow stopping for update (bool)
 variable "allow_stopping_for_update" {
   description = "If true, allows Terraform to stop the instance to update its properties"
-  default = "true"
+  default     = "true"
 }
 
 # Add special tags to the resources created by this module
 variable "tags" {
   description = "Add custom tags to all resources"
-  type    = "map"
-  default = {}
+  type        = "map"
+  default     = {}
 }
 
 # Format the hostname inputs are index+1, region, name_prefix
 variable "hostname_format" {
   description = "Format the hostname inputs are index+1, region, cluster_name"
-  default = "master-%[1]d-%[2]s"
+  default     = "master-%[1]d-%[2]s"
 }
 
 # Subnet ID
