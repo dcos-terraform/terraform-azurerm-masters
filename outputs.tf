9ad4116022c218dfc3c7f99df94983bdffdb0c85
Sebastian Brandt <793580+sebbrandt87@users.noreply.github.com>
2019-07-04 23:13:30 +0200

    updated build links, removed ip_configuration_names

--- outputs.tf
+++ outputs.tf
@@ -17,8 +17,3 @@ output "instance_nic_ids" {
   description = "List of instance nic ids created by this module"
   value       = ["${module.dcos-master-instances.instance_nic_ids}"]
 }
-
-output "ip_configuration_names" {
-  description = "List of ip configuration names associated with the instance nic ids"
-  value       = ["${module.dcos-master-instances.ip_configuration_names}"]
-}
