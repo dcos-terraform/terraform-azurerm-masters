5d89fd6b2058895be22647e4eaef948bed02a85b
Jan Ulferts <julferts@mesosphere.com>
2020-06-12 14:42:32 +0200

    fix: nested lists

--- outputs.tf
+++ outputs.tf
@@ -5,16 +5,15 @@ output "admin_username" {
 
 output "private_ips" {
   description = "List of private ip addresses created by this module"
-  value       = [module.dcos-master-instances.private_ips]
+  value       = module.dcos-master-instances.private_ips
 }
 
 output "public_ips" {
   description = "List of public ip addresses created by this module"
-  value       = [module.dcos-master-instances.public_ips]
+  value       = module.dcos-master-instances.public_ips
 }
 
 output "instance_nic_ids" {
   description = "List of instance nic ids created by this module"
-  value       = [module.dcos-master-instances.instance_nic_ids]
+  value       = module.dcos-master-instances.instance_nic_ids
 }
-
